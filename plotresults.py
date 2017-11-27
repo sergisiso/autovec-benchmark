050191d9798c0d4532f099029c20cf3034144c16
Sergi Siso <sergi.siso@stfc.ac.uk>
2017-11-27 10:02:03 +0000

    Updated plot results for new vspectrums and started latex table implementation

--- plotresults.py
+++ plotresults.py
@@ -31,15 +31,34 @@ def rgb(x):
     return [float(x[0])/255, float(x[1])/255, float(x[2])/255]
 
 palette = {
-        "Xeon-Icc" : rgb([240,163,255]),
-        "Xeon-Gcc": rgb([0,117,220]),
-        "Xeon-PGI": rgb([153,63,0]),
+        "Bdw-Icc" : rgb([240,163,255]),
+        "Bdw-Gcc": rgb([0,117,220]),
+        "Bdw-Pgi": rgb([153,63,0]),
         "Xeon-Clang": rgb([76,0,92]),
         "Pwr8-Xlc": rgb([25,25,25]),
         "Pwr8-Gcc": rgb([0,92,49]),
         "Pwr8-Pgi": rgb([43,206,72]),
         "Knl-Gcc": rgb([255,204,153]),
-        "Knl-PGI": rgb([128,128,128]),
+        "Knl-Icc": rgb([255,204,153]),
+        "Knl-Clang": rgb([128,128,128]),
+        "Linear Dependence" : rgb([240,163,255]),
+        "Induction Variable": rgb([0,117,220]),
+        "Global Data Flow": rgb([153,63,0]),
+        "Control Flow": rgb([76,0,92]),
+        "Symbolics": rgb([25,25,25]),
+        "Statement Reordering": rgb([0,92,49]),
+        "Loop Restructuring": rgb([43,206,72]),
+        "Node Splitting": rgb([255,204,153]),
+        "Expansion": rgb([128,128,128]),
+        "Crossing Thresholds" : rgb([240,163,255]),
+        "Reductions": rgb([0,117,220]),
+        "Recurrences": rgb([153,63,0]),
+        "Searching": rgb([76,0,92]),
+        "Packing": rgb([25,25,25]),
+        "Loop Rerolling": rgb([0,92,49]),
+        "Equivalencing": rgb([43,206,72]),
+        "Indirect Addressing": rgb([255,204,153]),
+        "Control Loops": rgb([128,128,128]),
     }
 
 categories = [
@@ -64,7 +83,7 @@ categories = [
 ]
 
 
-def add_box(ax, name, values, labels):
+def add_box(ax, name, values, labels, draw_mean=False):
 
     if len(list(values)) != len(list(labels)):
         print("Error: Inconsisten number of values/labels")
@@ -74,17 +93,17 @@ def add_box(ax, name, values, labels):
         if not np.isnan(value):
             ax.axhline(value, 0, 1, color=palette[label], label=label)
 
-    mean = np.mean([v for v in values if not np.isnan(v)])
-    #ax.axhline(mean,0,1,color='black',linestyle="--")
-    #ax.text(0.5, mean+0.05, "Avg. Mean = " + "{:.2f}".format(mean), ha='center', va='bottom', fontsize=10)
+    if draw_mean:
+        mean = np.mean([v for v in values if not np.isnan(v)])
+        ax.axhline(mean,0,1,color='black',linestyle="--")
+        ax.text(0.5, mean+0.05, "Avg. Mean = " + "{:.2f}".format(mean), ha='center', va='bottom', fontsize=8)
     ax.set_ylim(bottom=0, top=4)
     ax.tick_params(axis='x', which='both', bottom='off', top='off', labelbottom='off')
     ax.set_xlabel(name.title().replace("_","\n"), rotation=0)
 
-def plot_chart(charts, labels, values, outputfile, title= "Auto-vectorization", ylabel='Vector efficiency' ):
+def plot_chart(charts, labels, values, outputfile, title= "Auto-vectorization",
+        ylabel='Vector efficiency', connect=False, draw_mean=False ):
 
-    #print(values)
-    #print(charts)
     if len(list(values)) != len(list(charts)):
         print("Error: Inconsistent number of charts/values")
         exit(-1)
@@ -96,33 +115,39 @@ def plot_chart(charts, labels, values, outputfile, title= "Auto-vectorization", 
         ax.set_facecolor('none')
 
     for ax, c, v in zip(axis[:-1],list(charts),list(values)):
-        add_box(ax, c, v, labels) 
+        add_box(ax, c, v, labels, draw_mean) 
 
     for idx, (val,lab) in enumerate(sorted(zip(values[-1],labels))):
         v_loc = idx*(float(1)/len(labels))
-        #print(idx)
-        #print(len(labels))
         axis[-1].text(0.5,v_loc,lab, ha='left', va='center', fontsize=10)
-        xy = (1,val)
-        xy2 = (0.45,v_loc)
-        con = ConnectionPatch(xyA=xy, xyB=xy2, coordsA="data",coordsB="data", axesA=axis[-2], axesB=axis[-1], color=palette[lab], connectionstyle="arc,angleA=-180,angleB=-180,armA=-20,armB=20,rad=0")
-        axis[-2].add_artist(con)
+        if connect:
+            xy = (1,val)
+            xy2 = (0.45,v_loc)
+            con = ConnectionPatch(xyA=xy, xyB=xy2, coordsA="data",coordsB="data", axesA=axis[-2], axesB=axis[-1], color=palette[lab], connectionstyle="arc,angleA=-180,angleB=-180,armA=-20,armB=20,rad=0")
+            axis[-2].add_artist(con)
+        else:
+            con = ConnectionPatch( xyA=(0.25,v_loc), xyB=(0.45,v_loc),
+                           coordsA="data", coordsB="data",
+                           axesA=axis[-1], axesB=axis[-1],
+                           color=palette[lab])
+            axis[-1].add_artist(con)
+
     
 
     for ax in axis:
         ax.set_facecolor('none')
 
    
-
-    for val1, val2, lab in zip(values[0],values[1],labels):
-        con = ConnectionPatch( xyA=(1,val1), xyB=(0,val2),
+    if len(values) > 1 and connect:
+        #Connect same labels among inner charts (just 0 to 1 implemented)
+        for val1, val2, lab in zip(values[0],values[1],labels):
+            con = ConnectionPatch( xyA=(1,val1), xyB=(0,val2),
                            coordsA="data", coordsB="data",
                            axesA=axis[0], axesB=axis[1],
                            color=palette[lab])
-        axis[0].add_artist(con)
+            axis[0].add_artist(con)
     
     
-        #for ax, ax2, v, v2 in zip():
 
     # Remove all labels but leftmost
     axis[0].set_ylabel(ylabel)
@@ -195,15 +220,19 @@ def all_categories(data):
     return list(categories)
 
 
-def plot_compilers(data, output, title="", speedup_vs=None):
+def plot_compilers(data, output, architecture, title="", speedup_vs=None):
     # all compilers, all categories, original tsc
     vals = []
     labs = []
+    
+    compiler_list = [ x for x in data.keys() if architecture in x]
+    
     for cat in all_categories(data): 
         labs.append(cat)
         vals2 = []
-        for comp in ['icc_unsafe']:# data.keys():
-            par = 'original'
+        for comp in compiler_list :
+            par = 'None'
+
             if speedup_vs != None:
                 baseline = np.mean([x[0] for x in data[speedup_vs][cat][par].values()])
                 vals2.append(baseline / np.mean([x[0] for x in data[comp][cat][par].values()]))
@@ -213,13 +242,17 @@ def plot_compilers(data, output, title="", speedup_vs=None):
 
     vals = [list(i) for i in zip(*vals)]
     labs = [x.title().replace("_"," ") for x in labs]
-    char = ['icc 16']#[x.split("_")[0] for x in data.keys()]
-    plot_chart(char, labs , vals, output, title=title, ylabel = "Vector Efficiency")
+    char = [x[4:]  for x in compiler_list]
+
+    #print("charts",char)
+    #print("LABS", labs)
+    #print("vals", vals)
+    plot_chart(char, labs , vals, output, title=title, ylabel = "Vector Efficiency", connect=False, draw_mean=True)
 
 def plot_categories(data, comp, output, title="", speedup=False):
     labs = []
     vals = []
-    char = ('original', 'RUNTIME_INDEX',
+    char = ('None', 'RUNTIME_INDEX',
             'RUNTIME_CONDITIONS', 'RUNTIME_LOOP_BOUNDS', 'RUNTIME_ALL')
 
     for cat in data[comp].keys():
@@ -227,7 +260,7 @@ def plot_categories(data, comp, output, title="", speedup=False):
         vals2 = []
         for par in char:
             if speedup:
-                baseline = np.mean([x[0] for x in data[comp][cat]['original'].values()])
+                baseline = np.mean([x[0] for x in data[comp][cat]['None'].values()])
                 vals2.append( baseline / np.mean([x[0] for x in data[comp][cat][par].values()]) )
             else:
                 vals2.append(np.mean([x[2] for x in data[comp][cat][par].values()]))
@@ -235,7 +268,8 @@ def plot_categories(data, comp, output, title="", speedup=False):
 
     vals = [list(i) for i in zip(*vals)]
     labs = [x.title().replace("_"," ") for x in labs]
-    plot_chart(char, labs , vals, output, title=title, ylabel = "Vector Efficiency")
+
+    plot_chart(char, labs , vals, output, title=title, ylabel = "Vector Efficiency", connect=False, draw_mean=True)
 
 
 def plot_new(data, detailed_summary, parameter, output, title="", speedup=False):
@@ -258,7 +292,12 @@ def plot_new(data, detailed_summary, parameter, output, title="", speedup=False)
                 if (parameter not in test_sets) or (test in test_sets[parameter]): 
                     cat_none_sum = none_sum + value[2]
                     cat_none_count = none_count + 1
-            detailed_summary[compiler]['None'][category] = float(cat_none_sum/cat_none_count)
+
+            if(cat_none_count==0):
+                detailed_summary[compiler]['None'][category] = 0
+            else:
+                detailed_summary[compiler]['None'][category] = float(cat_none_sum/cat_none_count)
+
             none_sum = none_sum + cat_none_sum
             none_count = none_count + cat_none_count
 
@@ -268,7 +307,11 @@ def plot_new(data, detailed_summary, parameter, output, title="", speedup=False)
                 if (parameter not in test_sets) or (test in test_sets[parameter]): 
                     cat_par_sum = par_sum + value[2]
                     cat_par_count = par_count + 1
-            detailed_summary[compiler][parameter][category] = float(cat_par_sum/cat_par_count)
+            if(cat_par_count==0):
+                detailed_summary[compiler][parameter][category] = 0
+            else:
+                detailed_summary[compiler][parameter][category] = float(cat_par_sum/cat_par_count)
+
             par_sum = par_sum + cat_par_sum
             par_count = par_count + cat_par_count
 
@@ -276,31 +319,71 @@ def plot_new(data, detailed_summary, parameter, output, title="", speedup=False)
             print("Warrning!: " + str(compiler)+ " mismatching number of tests. Set to 0")
         if(none_count==0):
             print("Warrning!: " + str(compiler)+ " NONE contains no tests. Set to 0")
-            vals_rt.append(0)
+            vals_ct.append(0)
         else:
-            vals_rt.append(none_sum/none_count)
+            vals_ct.append(none_sum/none_count)
 
         if(par_count==0):
             print("Warrning!: " + str(compiler)+ " " + str(parameter) + " contains no tests. Set to 0")
-            vals_ct.append(0)
+            vals_rt.append(0)
         else:
-            vals_ct.append(par_sum/par_count)
+            vals_rt.append(par_sum/par_count)
 
     vals = [vals_ct,vals_rt]
        
     labs = [x.title() for x in labs]
-    plot_chart(char, labs , vals, output, title=title, ylabel = "Vector Efficiency")
+    plot_chart(char, labs , vals, output, title=title, ylabel = "Vector Efficiency", connect=True)
 
 
-def print_summary(detailed_summary):
+def print_summary(detailed_summary,categories):
     # Print latex table
+    #
+    # \begin{tabular}{ l | c | r }
+    #   \hline          
+    #     1 & 2 & 3 \\
+    #     4 & 5 & 6 \\
+    #     7 & 8 & 9 \\
+    #   \hline  
+    # \end{tabular}
 
-    print("Header: "+ str(categories))
-    for compiler in detailed_summary.keys():
-        print(compiler)
-        for parameter in detailed_summary[compiler].keys():
-            values = [str(x) + " " +str(detailed_summary[compiler][parameter][x]) for x in categories ]
-            print(parameter + " : " + str(values))
+    rotation = 90
+    cat_num = len(categories)
+    par_num = len(detailed_summary[next(iter(detailed_summary.keys()))])
+
+    with open('output.tex','w') as f:
+        #f.write("\\begin{table}[hpt] \n")
+        #f.write("\\centering \n")
+        f.write("\\begin{adjustbox}{center} \n")
+        f.write("\\begin{longtable}{|c|c" + ("|c"*(cat_num-5)) +"|}\n")
+        f.write("\\hline\n")
+        f.write("\\begin{turn}{"+str(rotation)+"}Architecture-Compiler\\end{turn} & ")
+        #f.write("\\begin{turn}{"+str(rotation)+"}Compiler\\end{turn} & ")
+        f.write("\\begin{turn}{"+str(rotation)+"}Parameters\\end{turn} ")
+        for cat in categories[:-5]:
+            f.write(" & \\begin{turn}{"+str(rotation)+"}"+cat.title().replace("_"," ") +"\\end{turn} ")
+
+        f.write("\\\\ \n")
+
+        for archcomp in sorted(detailed_summary.keys()):
+            arch, comp = archcomp.split("-")
+            #print(arch)
+            #print(comp)
+            f.write("\\hline \n")
+            f.write("  \multirow{"+str(par_num)+"}{*}{"+archcomp+"} & P "+ ("& 1.0 "*(cat_num-5))  +" \\\\ \n")
+            for i in range(par_num-1):
+                f.write(" & P "+ ("& 1.0 "*(cat_num-5))  +" \\\\ \n")
+        f.write("\\hline \n")
+        f.write("\\end{longtable}\n")
+        f.write("\\end{adjustbox}\n")
+
+
+        #f.write("{}")
+        #f.write("Header: "+ str(categories))
+        #for compiler in detailed_summary.keys():
+        #    f.write(compiler)
+        #    for parameter in detailed_summary[compiler].keys():
+        #        values = [str(x) + " " +str(detailed_summary[compiler][parameter][x]) for x in categories ]
+        #        f.write(parameter + " : " + str(values))
  
 def main():
 
@@ -329,22 +412,32 @@ def main():
                 load_data(data, compiler,category,parameters,parameters_path)
 
     print("")
-    print("Ploting charts...")
+
+    print("Ploting Parameters charts...")
     plot_new(data, detailed_summary, 'RUNTIME_INDEX','rindex.png', 'Index Parameters')
     plot_new(data, detailed_summary, 'RUNTIME_LOOP_BOUNDS','rbound.png', 'Loop Bounds Parameters')
     plot_new(data, detailed_summary, 'RUNTIME_CONDITIONS','rcond.png', 'Conditional Parameters')
     plot_new(data, detailed_summary, 'RUNTIME_ARITHMETIC','rarith.png', 'Arithmetic Parameters')
     plot_new(data, detailed_summary, 'RUNTIME_ALL','rall.png', 'All Parameters')
-    #print_summary(detailed_summary)
-    exit(0)
+    print_summary(detailed_summary, all_categories(data))
 
+    exit(0)
     print("- Compiler comparison")
-    plot_compilers(data, 'compilers.png', 'Compiler comparison')
-    exit()
-    plot_compilers(data, 'compilers_vs_gcc.png', 'Performance against gcc', speedup_vs='gcc_unsafe')
+    plot_compilers(data, 'compilers-bdw.png', 'bdw', 'Broadwell Compiler comparison')
+    plot_compilers(data, 'compilers-pwr8.png', 'pwr8', 'Power8 Compiler comparison')
+    plot_compilers(data, 'compilers_bdw_vs_gcc.png', 'bdw', 'Broadwell Performance against gcc', speedup_vs='bdw-gcc')
+    plot_compilers(data, 'compilers_pwr8_vs_gcc.png', 'pwr8', 'Power8 Performance against gcc', speedup_vs='pwr8-gcc')
 
     print("- Categories comparison")
-    plot_categories(data, 'icc_unsafe', 'icc.png', title="ICC Auto-vectorization", speedup=False)
+    plot_categories(data, 'bdw-icc', 'bdw-icc.png', title="BDW ICC Auto-vectorization", speedup=False)
+    plot_categories(data, 'bdw-gcc', 'bdw-gcc.png', title="BDW GCC Auto-vectorization", speedup=False)
+    plot_categories(data, 'bdw-pgi', 'bdw-pgi.png', title="BDW PGI Auto-vectorization", speedup=False)
+    plot_categories(data, 'pwr8-gcc', 'pwr8-gcc.png', title="Power8 GCC Auto-vectorization", speedup=False)
+    plot_categories(data, 'pwr8-xlc', 'pwr8-xlc.png', title="Power8 XLC Auto-vectorization", speedup=False)
+    plot_categories(data, 'knl-icc', 'knl-icc.png', title="KNL ICC Auto-vectorization", speedup=False)
+    #plot_categories(data, 'pwr8-pgi', 'pwr8-pgi.png', title="Power8 PGI Auto-vectorization", speedup=False)
+    #plot_categories(data, 'pwr8-clang', 'pwr8-clang.png', title="Power8 Clang Auto-vectorization", speedup=False)
+    exit(0)
     plot_categories(data, 'icc_unsafe', 'icc_speedup.png', title="ICC performance angainst original", speedup=True)
     plot_categories(data, 'gcc_unsafe', 'gcc.png', title="GCC Auto-vectorization", speedup=False)
     plot_categories(data, 'gcc_unsafe', 'gcc_speedup.png', title="GCC performance angainst original", speedup=True)
