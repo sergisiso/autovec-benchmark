465a23a98836ada458cb076dc79d10a0db4b8953
Sergi Sisó <sergiesg@gmail.com>
2020-06-16 18:29:40 +0100

    Update microkernels common.mk using the mandelbrot as example

--- microkernels/common.mk
+++ microkernels/common.mk
@@ -24,77 +24,98 @@
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Choose Vector ISA
-
 ifeq ($(VECTOR_ISA),avx2)
-	GNUISA=-march=skylake
-	CLANGISA=-march=skylake
-	INTELISA=AVX2
+	GNUISA=-mtune=avx2
+	CLANGISA=-mtune=avx2
+	INTELISA=-xAVX2
 	PGIISA=-tp=haswell
 else ifeq ($(VECTOR_ISA),avx512)
-	GNUISA=-march=skylake-avx512 -mprefer-vector-width=512
-	CLANGISA=-march=skylake-avx512
-	INTELISA=CORE-AVX512 -qopt-zmm-usage=high
+	GNUISA=-mtune=skylake-avx512 -mprefer-vector-width=512
+	CLANGISA=-mtune=skylake-avx512 -mprefer-vector-width=512
+	INTELISA=-xCORE-AVX512 -qopt-zmm-usage=high
 	PGIISA=-tp=skylake
 else ifeq ($(VECTOR_ISA),knl)
-	GNUISA=-march=knl
-	CLANGISA=-march=knl
-	INTELISA=MIC-AVX512
+	GNUISA=-mtune=knl
+	CLANGISA=-mtune=knl
+	INTELISA=-xMIC-AVX512
 	PGIISA=-tp=knl
 else ifeq ($(VECTOR_ISA),altivec)
 	GNUISA=-mcpu=power8
 	CLANGISA=-mcpu=power8
 	PGIISA=
 else
-	# NO ISA specified
-	#$(info asdf)
-	GNUISA=-march=native
-	INTELISA=Host
+	# NO ISA specified - use native one
+	VECTOR_ISA=native
+	GNUISA=-mtune=native
+	INTELISA=-xHost
 endif
 
-
-# Choose compiler toolchain
+# Choose compiler toolchain - gnu by default
+ifndef $(TEST_COMPILER)
+	TEST_COMPILER := gnu
+endif
+IDSTRING := $(TEST_COMPILER)_$(VECTOR_ISA)_
 ifeq ($(TEST_COMPILER),gnu)
 	CC=g++
 	CFLAGS=-O3 $(GNUISA) -std=c++11
 	UNSAFE=-ffast-math
-	NOVEC=-fno-tree-vectorize
-	REPORT=-fopt-info-optimized=optreports/gcc_$(VECTOR_ISA)_
-	ASM=-S
+	NOVECFLAG=-fno-tree-vectorize
+	REPORT=-fopt-info-optimized=$(IDSTRING)report.txt
+	ASM=-S -o $(IDSTRING)
 else ifeq ($(TEST_COMPILER),intel)
 	CC=icpc 
-	CFLAGS=-O3 -x$(INTELISA) -std=c++11
+	CFLAGS=-O3 $(INTELISA) -std=c++11
 	UNSAFE=-fp-model fast=2
-	NOVEC=-no-simd -no-vec
-	REPORT=-qopt-report=5 -qopt-report-file=optreports/intel_$(VECTOR_ISA)_
+	NOVECFLAG=-no-simd -no-vec
+	REPORT=-qopt-report=5 -qopt-report-file=$(IDSTRING)report.txt
 	ASM=-S
 else ifeq ($(TEST_COMPILER),pgi)
 	CC=pgc++
 	CFLAGS=-O3 $(PGIISA) --c++11
 	UNSAFE=-fast -fastsse
-	NOVEC=-Mnovect
-	REPORT=-Minfo=all -D
+	NOVECFLAG=-Mnovect
+	REPORT=-Minfo=all
 	ASM=-S
 else ifeq ($(TEST_COMPILER),ibm)
 	CC=xlc++
 	CFLAGS=-O3 -std=c++11
 	UNSAFE= -qhot=fastmath
-	NOVEC=-qnoaltivec -qhot=novector:fastmath -qsimd=noauto
+	NOVECFLAG=-qnoaltivec -qhot=novector -qsimd=noauto
 	REPORT=-D
 	ASM= -S
 else ifeq ($(TEST_COMPILER),clang)
 	CC=clang
 	CFLAGS=-O3 $(CLANGISA) -std=c++11 -lm -lstdc++
 	UNSAFE=-ffast-math
-	NOVEC=-fno-vectorize
-	REPORT=-fsave-optimization-record -foptimization-record-file=optreports/clang_$(VECTOR_ISA)_
+	NOVECFLAG=-fno-vectorize
+	REPORT=-fsave-optimization-record -foptimization-record-file=$(IDSTRING)report.txt
 	ASM=-S
 else
-	CC=icpc
-	CFLAGS=-O3 -xHost -std=c++11
-	UNSAFE=-fp-model fast=2 -prec-sqrt -ftz -fma
-	NOVEC=-no-simd -no-vec
-	REPORT=-qopt-report=5
-	ASM=-Fa
-	#$(error No compiler specified)
-	# NO COMPILER SPECIFIED
+	$(error Unrecognized compiler $(TEST_COMPILER))
 endif
+
+
+# Disable vectorization if requested
+ifndef $(NOVEC)
+	NOVEC := no
+endif
+
+ifeq ($(NOVEC),yes)
+	CFLAGS += $(NOVECFLAG)
+	VECSTRING := novec
+else
+	VECSTRING := vec
+endif
+
+# Add runtime-invariants manual specialization 
+ifndef $(SPECIALIZE)
+	SPECIALIZE := no
+endif
+
+ifeq ($(SPECIALIZE),yes)
+	CFLAGS += -DSPECIALIZE
+	SPECSTRING := specialized
+else
+	SPECSTRING := rt
+endif
+
